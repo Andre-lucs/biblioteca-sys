83b3155a29ff028200b5b388c356f9f8dd6deeee
Andre-lucs <andreltd312@gmail.com>
2023-09-21 15:23:05 -0300

    Created Repo

---
+++ src/test/java/com/andrelucs/bibliotecasys/BibliotecaSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrelucs.bibliotecasys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
